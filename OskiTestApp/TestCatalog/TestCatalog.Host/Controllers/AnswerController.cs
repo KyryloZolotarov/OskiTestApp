536d8818cc287a2691d54604ffcb7df84ab87696
Kyrylo Zolotarov <zlatashay@gmail.com>
2024-02-07 04:49:59 +0200

    CRUD operations for Answers in TestCatalog added

---
+++ OskiTestApp/TestCatalog/TestCatalog.Host/Controllers/AnswerController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using TestCatalog.Host.Models.Requests;
+using TestCatalog.Host.Services;
+using TestCatalog.Host.Services.Interfaces;
+
+namespace TestCatalog.Host.Controllers
+{
+    [Route("answer")]
+    [ApiController]
+    public class AnswerController : ControllerBase
+    {
+        private readonly IAnswerService _answerService;
+
+        public AnswerController(IAnswerService answerService)
+        {
+            _answerService = answerService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> AddAnswerAsync([FromBody] AddAnswerRequest answer)
+        {
+            await _answerService.AddAnswerAsync(answer);
+            return Ok();
+        }
+
+        [HttpPut]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateAnswerAsync([FromBody] UpdateAnswerRequest answer)
+        {
+            await _answerService.UpdateAnswerAsync(answer);
+            return Ok();
+        }
+
+        [HttpDelete]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteAnswerAsync([FromQuery] int id)
+        {
+            await _answerService.DeleteAnswerAsync(id);
+            return Ok();
+        }
+    }
+}
---
+++ OskiTestApp/TestCatalog/TestCatalog.Host/Services/Interfaces/IAnswerService.cs
@@ -0,0 +1,11 @@
+﻿using TestCatalog.Host.Models.Requests;
+
+namespace TestCatalog.Host.Services.Interfaces
+{
+    public interface IAnswerService
+    {
+        Task AddAnswerAsync(AddAnswerRequest answer);
+        Task UpdateAnswerAsync(UpdateAnswerRequest answer);
+        Task DeleteAnswerAsync(int answerId);
+    }
+}
